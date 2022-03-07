5f589f97ac529d8838de762b9eae0fc6941b0bfb
Duongmanhvp <dinhmanhbh03@gmail.com>
2022-03-07 15:05:55 +0700

    Update Guesslt.cpp
    thêm hàm srand thư viện ctime để sinh số ngẫu nhiên khác nhau

--- Guesslt.cpp
+++ Guesslt.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
+     srand(time(0));
      int randomNumber = rand() % 100 + 1;
-     ;
      int number;
 
      do
